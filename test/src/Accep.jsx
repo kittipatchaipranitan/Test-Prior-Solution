4231a4303d25a8b25b96d27ac2f65ba6ed79771a
kittipatchaipranitan <s6404062620176@email.kmutnb.ac.th>
2024-02-21 00:53:09 +0700

    Test Fronend

--- test/src/Accep.jsx
+++ test/src/Accep.jsx
@@ -3,27 +3,37 @@ import { useLocation, Link } from 'react-router-dom';
 
 const Acep = () => {
     const location = useLocation();
-    const { data } = location.state || { name: '', id: '', sex: '', date: '' };
+
+    const { data, time } = location.state || { data: { name: '', id: '', sex: '' }, time: '' };
+
+    const parts = time.split('-');
+    const year = parseInt(parts[0], 10);
+    const b_year = year + 543;
+    const month = parts[1].padStart(2, '0');
+    const day = parts[2].padStart(2, '0');
+    const formattedDate = `${day}-${month}-${b_year}`;
+
     const formatID = (id) => {
         if (!id) return '';
         return `${id.substring(0, 1)}-${id.substring(1, 5)}-${id.substring(5, 10)}-${id.substring(10, 12)}-${id.substring(12)}`;
     };
 
-    function permissions(data) {
+    function permissions(time) {
         const startDate = new Date('2023-06-01');
         const endDate = new Date('2023-08-31');
 
-        if (!data || !data.date) {
-            return 'data or data.date is undefined';
+        if (!time) {
+            return 'undefined';
         }
 
-        const dataDateObj = new Date(data.date);
+        const dataDateObj = new Date(time);
         dataDateObj.setFullYear(dataDateObj.getFullYear() + 65);
 
+
         if (dataDateObj <= endDate) {
-            return 'working';
+            return <span class="text-green-500">สามารถเข้ารับบริการได้</span>;
         } else {
-            const originalDataDate = new Date(data.date);
+            const originalDataDate = new Date(time);
             const sixMonthsBeforeStartDate = new Date(startDate);
             sixMonthsBeforeStartDate.setMonth(startDate.getMonth() - 6);
             const twoYearsAfterStartDate = new Date(startDate);
@@ -31,44 +41,52 @@ const Acep = () => {
 
             if ((originalDataDate >= sixMonthsBeforeStartDate && originalDataDate <= startDate) ||
                 (originalDataDate > startDate && originalDataDate <= twoYearsAfterStartDate)) {
-                return <span className="text-green-500">สามารถเข้ารับบริการได้</span>;
+                return <span class="text-green-500">สามารถเข้ารับบริการได้</span>;
             } else {
-                return <span className="text-red-500">ไม่สามารถเข้ารับบริการได้</span>;
+                if (sixMonthsBeforeStartDate > endDate) {
+                    return <span class="text-red-500">ไม่สามารถเข้ารับบริการได้เนื่องจากอายุจะครบ 6 เดือน วันที่ {sixMonthsBeforeStartDate.getDate()}/{sixMonthsBeforeStartDate.getMonth() + 1}/{sixMonthsBeforeStartDate.getFullYear() + 543} </span>;
+                }
+                return <span class="text-red-500">ไม่สามารถเข้ารับบริการได้เนื่องจากอายุจะครบ 65 ปี วันที่ {dataDateObj.getDate()}/{dataDateObj.getMonth() + 1}/{dataDateObj.getFullYear() + 543} </span>;
+
             }
         }
     }
 
     return (
         <div className="relative flex justify-center items-center min-h-screen bg-no-repeat bg-cover bg-center bg-[url('https://www.cdg.co.th/website/wp-content/uploads/2023/05/Press-Release-Vaccine.webp')] font-sarabun">
-            <div className="absolute inset-0 bg-white opacity-20"></div>
-            <div className="container max-w-full sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl p-4 md:p-6 bg-white rounded-lg shadow-lg z-10">
-                <h1 className="text-center text-xl sm:text-2xl md:text-3xl font-semibold text-[#49B1DD] mb-4 md:mb-8">ข้อมูลการลงทะเบียน</h1>
-                <div className="space-y-4">
-                    <div>
-                        <label className="text-[#49B1DD]">ชื่อ-นามสกุล</label>
-                        <p className="p-2 bg-[#49B1DD]/20 rounded-md">{data.name}</p>
-                    </div>
-                    <div>
-                        <label className="text-[#49B1DD]">เลขบัตรประชาชน</label>
-                        <p className="p-2 bg-[#49B1DD]/20 rounded-md">{formatID(data.id)}</p>
-                    </div>
-                    <div>
-                        <label className="text-[#49B1DD]">เพศ</label>
-                        <p className="p-2 bg-[#49B1DD]/20 rounded-md">{data.sex}</p>
-                    </div>
-                    <div>
-                        <label className="text-[#49B1DD]">วัน/เดือน/ปีเกิด</label>
-                        <p className="p-2 bg-[#49B1DD]/20 rounded-md">{data.date}</p>
-                    </div>
+        <div className="absolute inset-0 bg-white opacity-20"></div>
+        {/* Adjust max-width and padding for different screens */}
+        <div className="container max-w-full md:max-w-md p-4 md:p-6 bg-white rounded-lg shadow-lg z-10 relative">
+            <h1 className="text-center text-2xl md:text-3xl font-semibold text-[#49B1DD] mb-4 md:mb-8">ข้อมูลการลงทะเบียน</h1>
+            <div className="registration-info space-y-4">
+                {/* Responsive text sizes and paddings */}
+                <div className="info-item">
+                    <label className="text-[#49B1DD]">ชื่อ-นามสกุล</label>
+                    <p className="p-2 bg-[#49B1DD]/20 rounded-md">{data.name}</p>
                 </div>
-                <div className='text-center text-lg sm:text-xl md:text-2xl mt-2 text-[#5C8A9E] font-bold'>
-                    {permissions(data)}
+                <div className="info-item">
+                    <label className="text-[#49B1DD]">เลขบัตรประชาชน</label>
+                    <p className="p-2 bg-[#49B1DD]/20 rounded-md">{formatID(data.id)}</p>
                 </div>
-                <div className="text-center mt-8">
-                    <Link to="/" className="ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 inline-block w-full bg-[#3389B0] text-white font-bold py-2 sm:py-3 px-4 rounded-md hover:bg-[#46AAD5] transition duration-300">กลับหน้าหลัก</Link>
+                <div className="info-item">
+                    <label className="text-[#49B1DD]">เพศ</label>
+                    <p className="p-2 bg-[#49B1DD]/20 rounded-md">{data.sex}</p>
+                </div>
+                <div className="info-item">
+                    <label className="text-[#49B1DD]">วัน/เดือน/ปีเกิด</label>
+                    <p className="p-2 bg-[#49B1DD]/20 rounded-md">{formattedDate}</p>
                 </div>
             </div>
+            <div className='text-center text-xl md:text-2xl mt-2 text-[#5C8A9E] font-bold'>
+                {permissions(time)}
+            </div>
+            {/* Adjust button size and spacing */}
+            <div className="text-center mt-4 md:mt-8">
+                <Link to="/" className="ease-in-out delay-150 over:-translate-y-1 hover:scale-90 inline-block w-full bg-[#3389B0] text-white font-bold py-2 md:py-3 px-4 md:px-6 rounded-md hover:bg-[#46AAD5] transition duration-300">กลับหน้าหลัก</Link>
+            </div>
         </div>
+    </div>
+    
     );
 };
 
--- test/src/App.jsx
+++ test/src/App.jsx
@@ -1,24 +1,44 @@
 import React, { useState } from 'react';
 import "./App.css"
 import { useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2'
+import { ThaiDatePicker } from "thaidatepicker-react";
 
 function App() {
-  const [data, setData] = useState({ name: '', id: '', sex: '', date: '' });
-  const [errors, setErrors] = useState({});
+
   const navigate = useNavigate();
+  const [time, setTime] = useState('');
+  const [data, setData] = useState({ name: '', id: '', sex: '' });
+  const [errors, setErrors] = useState({});
+
+  const currentDate = new Date().toLocaleDateString();
+  const parts = currentDate.split('/');
+  const year = parts[2];
+  const month = parts[0].padStart(2, '0');
+  const day = parts[1].padStart(2, '0');
+  const formattedDate = `${year}-${month}-${day}`;
+
+  const handleWatDatePickerChange = async (christDate, burddhistDate) => {
+    console.log(christDate)
+    console.log(burddhistDate);
+    setTime(burddhistDate)
+    setTime(christDate)
+
+  }
 
   const handleSubmit = (event) => {
     event.preventDefault();
     if (validateForm()) {
       console.log('Form submitted successfully:', data);
-      navigate('/Accep', { state: { data } });
+      navigate('/Accep', { state: { data, time } });
     } else {
       console.log('Form submission failed due to validation errors.');
     }
   };
 
   const resetValue = () => {
-    setData({ name: '', id: '', sex: '', date: '' });
+    setData({ name: '', id: '', sex: '', });
+    console.log('resetError');
     setErrors({});
   };
 
@@ -27,22 +47,42 @@ function App() {
     const errors = {};
 
     if (!data.name.trim()) {
+      Swal.fire({
+        title: "คำเตือน",
+        text: "กรุณากรอกชื่อ-นามสกุล",
+        icon: "warning"
+      });
       errors.name = 'กรุณากรอกชื่อ-นามสกุล';
       isValid = false;
     }
 
-    if (!data.id.trim()) {
+    else if (!data.id.trim()) {
+      Swal.fire({
+        title: "คำเตือน",
+        text: "กรุณากรอกเลขบัตรประชาชน",
+        icon: "warning"
+      });
       errors.id = 'กรุณากรอกเลขบัตรประชาชน';
       isValid = false;
     }
 
-    if (!data.sex) {
+    else if (!data.sex) {
+      Swal.fire({
+        title: "คำเตือน",
+        text: "กรุณาเลือกเพศ",
+        icon: "warning"
+      });
       errors.sex = 'กรุณาเลือกเพศ';
       isValid = false;
     }
 
-    if (!data.date) {
-      errors.date = 'กรุณากรอกวัน/เดือน/ปี';
+    else if (!time) {
+      Swal.fire({
+        title: "คำเตือน",
+        text: "กรุณากรอกวัน/เดือน/ปี",
+        icon: "warning"
+      });
+      errors.time = 'กรุณากรอกวัน/เดือน/ปี';
       isValid = false;
     }
 
@@ -55,6 +95,7 @@ function App() {
       <div className="absolute inset-0 bg-white opacity-20"></div>
       <div className="container max-w-full md:max-w-xl lg:max-w-2xl xl:max-w-3xl p-4 md:p-6 bg-white rounded-lg shadow-lg z-10 relative">
         <form onSubmit={handleSubmit}>
+
           <h1 className="text-center text-2xl md:text-3xl font-semibold text-[#49B1DD] mb-4 md:mb-8">ลงทะเบียนรับวัคซีน</h1>
           <div className="mb-4">
             <label className="block text-[#49B1DD]">ชื่อ-นามสกุล</label>
@@ -99,18 +140,23 @@ function App() {
 
           <div className="mb-4">
             <label className="block text-[#49B1DD]">วัน/เดือน/ปีเกิด</label>
-            <input
-              type='date'
-              name='date'
-              className="w-full p-2 bg-[#49B1DD]/20 rounded-md"
-              value={data.date}
-              onChange={(e) => setData({ ...data, date: e.target.value })}
+
+            <ThaiDatePicker
+              clearable={false}
+              inputProps={{
+                displayFormat: "DD/MM/YYYY",
+                className: "w-full p-2 bg-[#49B1DD]/20 rounded-md"
+              }}
+              value={time}
+              onChange={handleWatDatePickerChange}
+              maxDate={formattedDate}
             />
             {errors.date && <div style={{ color: 'red' }}>{errors.date}</div>}
           </div>
 
           <button type='submit' className="w-full bg-[#3389B0] text-white font-bold py-2 px-4 rounded-md hover:bg-[#46AAD5] transition duration-300 mb-2">ยืนยัน</button>
           <button type='button' onClick={resetValue} className="w-full bg-[#3389B0] text-white font-bold py-2 px-4 rounded-md hover:bg-[#46AAD5] transition duration-300">ล้างค่า</button>
+
         </form>
       </div>
     </div>
